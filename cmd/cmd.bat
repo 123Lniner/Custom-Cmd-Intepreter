c303924411cceeed42326a6c30b147fc8f35bb7f
123Lniner <164357272+123Lniner@users.noreply.github.com>
2024-07-04 18:32:33 -0400

    Update cmd.bat

--- cmd/cmd.bat
+++ cmd/cmd.bat
@@ -1,15 +1,20 @@
 @echo off
 title Command Prompt
-set path=C:\
+set path=%systemroot%\system32\
 
 :cmdver
-echo Welcome to 123Lniner's command interpreter [Version 2024.7.4]
+echo Welcome to 123Lniner's command interpreter [Version 2024.7.4.11]
 echo See "..\LICENSE" for license information.
 echo.
 goto main
 
 :exit
 exit
+
+:cls
+cls
+echo.
+goto main
 
 :run
 echo Please type the name of the file you want to run
@@ -27,7 +32,7 @@ echo.
 goto main
 
 :display
-echo Please type the thing you want to echo
+echo Please type the thing you want to repeat
 echo.
 set /p text="DISPLAY> "
 echo %text%
@@ -38,6 +43,7 @@ goto main
 echo DISPLAY        Displays text of next line.
 echo SETDIR         Changes the current directory to the path specified in the following line.
 echo RUN            Runs a program specified in the following line (use SETDIR to change folders).
+echo CLS            Clears the screen.
 echo EXIT           Quits the command interpreter.
 echo CMDVER         Displays current command interpreter version.
 echo.
@@ -53,6 +59,8 @@ if %cmd% EQU setdir call :setdir
 if %cmd% EQU SETDIR call :setdir
 if %cmd% EQU run call :run
 if %cmd% EQU RUN call :run
+if %cmd% EQU cls call :cls
+if %cmd% EQU CLS call :cls
 if %cmd% EQU exit call :exit
 if %cmd% EQU EXIT call :exit
 if %cmd% EQU cmdver call :cmdver
